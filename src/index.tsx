3fb60e856bc6f07dafe636aeaea46a79df7449ec
jhlov <jhlovv@gmail.com>
2022-07-21 23:45:50 +0900

    로그인 버튼

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -5,6 +7,22 @@ import store from "store";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,46 @@
+import GoogleIcon from "@mui/icons-material/Google";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
+import { Button } from "react-bootstrap";
+import "./Login.scss";
 
 const Login = () => {
-  return <div>로그인</div>;
+  const provider = new GoogleAuthProvider();
+
+  const onLogin = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        console.log(credential);
+        const token = credential?.accessToken;
+        console.log(token);
+
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+        // ...
+      })
+      .catch(error => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
+  return (
+    <div className="login">
+      <Button variant="outline-primary" onClick={onLogin}>
+        <GoogleIcon /> Sign in with Google
+      </Button>
+    </div>
+  );
 };
 
 export { Login };
