a55ad9fcbbca3a2a9539f803ebb15086e084bba0
jhlov <jhlovv@gmail.com>
2022-07-23 23:53:04 +0900

    api 호출 시 로딩 표시

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,13 @@ import axios from "axios";
 import { getAuth } from "firebase/auth";
 import { isEmpty } from "lodash";
 import qs from "qs";
+import store from "store";
+import { setLoading } from "store/common";
 
 export const api = {
-  get: <T>(url: string, params?: object) => {
+  get: async <T>(url: string, params?: object) => {
+    store.dispatch(setLoading(true));
+
     const auth = getAuth();
 
     let newUrl = isEmpty(params) ? url : `${url}?${qs.stringify(params)}`;
@@ -16,6 +20,11 @@ export const api = {
       }
     };
 
-    return axios.get<T>(newUrl, headers);
+    try {
+      const r = await axios.get<T>(newUrl, headers);
+      return Promise.resolve(r);
+    } finally {
+      store.dispatch(setLoading(false));
+    }
   }
 };
